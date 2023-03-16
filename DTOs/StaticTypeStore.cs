3c03016935ea2a1c392397417f69fdcc5a7a3c8c
NareeyaChenu <nachenu@gmail.com>
2023-03-16 10:46:29 +0700

    upload orther file

--- DTOs/StaticTypeStore.cs
+++ DTOs/StaticTypeStore.cs
@@ -3,5 +3,7 @@ namespace static_sv.DTOs
     public class StaticTypeStore
     {
         public static string? Image = "image";
+        public static string Video = "video";
+        public static string File = "file";
     }
 }
